d69d96161422ea0e498bde84558a224615010b05
Jeremiah Bargar <bullfrog@runbox.com>
2016-04-21 23:57:06 -0400

    minor cleanup, code comments, updated screenshot after template changes

--- app/home/controller.js
+++ app/home/controller.js
@@ -13,22 +13,38 @@ export default Ember.Controller.extend({
     };
   }),
   max: Ember.computed.max('data'),
-  waterfill: Ember.computed('data.[]', function() {
+
+  // calculates the water that could be contained in the columns
+  waterfillByLevel: Ember.computed('data.[]', function() {
     let data = this.get('data');
 
+    // total amount of water for whole histogram
     let waterfill = [];
+
+    // start at highest column(s)' level and calculate fill a row at a time
     for (let i = this.get('max'); i > 0; i--) {
 
+      // amount of water for current level (row) of the histogram
       let levelfill = 0;
+
+      // number of columns at/above the current level
       let count = 0;
+
+      // index of leftmost column at/above the current level
       let minIndex;
+
+      // index of rightmost column at/above the current level
       let maxIndex = -1;
+
       data.forEach((datum, index) => {
         if (datum >= i) {
+
+          // this column is at or above the current level, include
 
           count++;
 
           if (minIndex === undefined) {
+            // first column at/above is set as leftmost column
             minIndex = index;
           }
 
@@ -38,7 +54,7 @@ export default Ember.Controller.extend({
         }
       });
 
-      console.log(`level ${i}: count: ${count} min: ${minIndex} max: ${maxIndex}`)
+      // if there are 0 or 1 columns at/above current level, no water can fill
       if (count > 1) {
 
         levelfill = maxIndex - minIndex - count + 1;
@@ -48,18 +64,19 @@ export default Ember.Controller.extend({
     }
     return waterfill;
   }),
-  waterfillTotal: Ember.computed.sum('waterfill'),
+  waterfill: Ember.computed.sum('waterfillByLevel'),
   actions: {
+
+    // limited randomization of data to generate a new histogram whose fill
+    // can be calculated
     randomize: function() {
       let data = [];
       for (let i = 0; i < 7; i++) {
         var datum = Math.floor(Math.random() * (8));
-        console.log('random: ' + datum);
         data.push(datum);
       }
-      console.log('random data: ' + data);
       let dataString = JSON.stringify(data, null, '');
-      this.set('dataString', dataString.substring(1, dataString.length - 1))
+      this.set('dataString', dataString.substring(1, dataString.length - 1));
     }
   }
 });
